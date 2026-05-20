4c99ec36b29810a1509496ec7b9833abe94da233
Sarthak-Chaubey <sarthakchaubey07@gmail.com>
2026-05-20 22:51:15 +0530

    Added frontend dashboard UI

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('DevSecOps App Running 🚀');
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3000, () => {
-    console.log('Server running on port 3000');
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
